573b159b9faba8ade00efe47f66184286b1d736b
Josh Dennis <jdphd7@gmail.com>
2016-10-29 11:49:25 -0700

    Added Pokemon.h and split up some of the functionality. Not sure if working yet

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -1,44 +1,28 @@
 #include <iostream>
 #include <string>
+#include "Pokemon.h"
 using namespace std;
-class Pokemon
-{
-private:
-string name;
-int level;
-int hp, hpMax;
-int attack,defense,specialAttack,specialDefense,speed;
-public:
 
-Pokemon(int index)
-{
-}
-
-~Pokemon()
-{
-}
-
-string get_name()
+string Pokemon::get_name()
 {
 	return name;
 }
 
-int get_level()
+int Pokemon::get_level()
 {
 	return level;
 }
 
-int get_hpMax()
+int Pokemon::get_hpMax()
 {
 	return hpMax;
 }
 
-int get_hp()
+int Pokemon::get_hp()
 {
 	return hp;
 }
 
-};
 int main()
 {
 	return 0;
---
+++ Pokemon.h
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+using namespace std;
+enum class element{Fire, Water,Grass};
+class Pokemon
+{
+private:
+string name;
+int level=1;
+int hp=20, hpMax=10;
+int attack=10,defense=10,specialAttack=10,specialDefense=10,speed=10;
+public:
+
+Pokemon(int index)
+{
+}
+
+~Pokemon()
+{
+}
+
+string name;
+int level;
+int hp, hpMax;
+int attack,defense,specialAttack,specialDefense,speed;
+public:
+
+Pokemon(int index)
+{
+}
+
+~Pokemon()
+{
+}
+friend *Pokemon make_pokemon(element tempElement, string tempName);
+//Getter Functions
+string get_name();
+int get_level();
+int get_hpMax();
+int get_hp();
+};
+
+class Fire: public Pokemon
+{
+	hp++;
+	attack+=2;
+	defense-=2;
+	specialAttack+=4;
+	specialDefense-=2;
+	speed+=2;
+}
+
+class Water: public Pokemon
+{
+	hp+=2;
+	attack-=2;
+	defense+=2;
+	specialAttack-=1;
+	specialDefense+=4;
+	speed-=1;
+}
+class Grass: public Pokemon
+{
+	hp-=3;
+	attack-=2;
+	defense+=0;
+	specialAttack-=3;
+	specialDefense+=0;
+	speed-=2;
+}
+int main()
+{
+	return 0;
+}
